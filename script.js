3ead4bdb2d8837823444ab7a93fabb45fb29a064
Murilo Medeiros <murilo1988.dev@gmail.com>
2023-11-16 22:41:15 -0300

    JS
    Inicio da configuração do menu.

---
+++ script.js
@@ -0,0 +1,19 @@
+let i = window.document.getElementById('res')
+function inicio() {    
+    i.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci fugiat quia officiis perferendis pariatur deserunt nihil a, numquam autem architecto eveniet? Aliquam quo ex sed voluptatibus aspernatur accusamus. Odit, quidem'
+    i.style.padding = '10px'
+    i.style.textIndent = '20px'
+}
+function sobre() {
+    i.innerText = 'Sobre'
+    i.    
+}
+function formacao() {
+    i.innerText = 'Fromação'    
+}
+function portfolio() {
+    i.innerText = 'Portfólio'    
+}
+function contato() {
+    i.innerText = 'Contato'    
+}
